d9ae82e13a76bee3908458a6418024c2fdce31a0
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-01-17 20:04:05 -0300

    Refatoração exercicio 2 usando função para imprimir

--- src/exercicio02.js
+++ src/exercicio02.js
@@ -1,33 +1,30 @@
-const readline = require('readline-sync');
+const { questionFloat } = require('readline-sync');
+const { imprimeValoresParcela } = require('./function-utils')
 
-const valorDivida = readline.questionFloat("Informe o valor da dívida: ");
+
+const valorDivida = questionFloat("Informe o valor da dívida: ");
 
 console.log('Vlr Dívida\tVlr Juros\tNum Parcelas\tVlr Parcela');
 
-for (let i = 0; i <= 12; i = i + 3) {
-  let valorTotalDivida = 0;
-  let valorJuros = 0;
-  let valorParcela = 0;
-  if (i == 0) {
-    valorTotalDivida = valorDivida;
-    valorParcela = valorDivida;
-    console.log(`${valorTotalDivida}\t${valorJuros}\t${i}\t${valorParcela.toFixed(2)}`)
-  } else if (i == 3) {
-    valorTotalDivida = (valorDivida * 0.10) + valorDivida;
-    valorParcela = valorTotalDivida / i;
-    console.log(`${valorTotalDivida}\t${valorJuros}\t${i}\t${valorParcela.toFixed(2)}`)
-  } else if (i == 6) {
-    valorTotalDivida = (valorDivida * 0.15) + valorDivida;
-    valorParcela = valorTotalDivida / i;
-    console.log(`${valorTotalDivida}\t${valorJuros}\t${i}\t${valorParcela.toFixed(2)}`)
-  } else if (i == 9) {
-    valorTotalDivida = (valorDivida * 0.20) + valorDivida;
-    valorParcela = valorTotalDivida / i;
-    console.log(`${valorTotalDivida}\t${valorJuros}\t${i}\t${valorParcela.toFixed(2)}`)
-  } else if (i == 12) {
-    valorTotalDivida = (valorDivida * 0.25) + valorDivida;
-    valorParcela = valorTotalDivida / i;
-    console.log(`${valorTotalDivida}\t${valorJuros}\t${i}\t${valorParcela.toFixed(2)}`)
-  }
+let percentualJuros = 0;
+let numParcelas = 0;
 
+for (let parcela = 0; parcela <= 12; parcela = parcela + 3) {
+  if (parcela == 0) {
+    numParcelas = parcela + 1;
+    percentualJuros = 0;
+  } else if (parcela == 3) {
+    numParcelas = parcela;
+    percentualJuros = 10;
+  } else if (parcela == 6) {
+    numParcelas = parcela;
+    percentualJuros = 15;
+  } else if (parcela == 9) {
+    numParcelas = parcela;
+    percentualJuros = 20;
+  } else if (parcela == 12) {
+    numParcelas = parcela;
+    percentualJuros = 25;
+  }
+  imprimeValoresParcela(valorDivida, numParcelas, percentualJuros);
 }
